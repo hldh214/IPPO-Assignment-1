25b1a2599060d4d918b7c698b35ddd1f1aba5bd1
hldh214 <hldh214@gmail.com>
2020-10-23 09:49:58 +0800

    perf(main): remove unused stubs

--- src/main/java/ippo/assignment1/Main.java
+++ src/main/java/ippo/assignment1/Main.java
@@ -5,9 +5,6 @@ import ippo.assignment1.library.PictureViewer;
 public class Main {
 
     public static void main(String[] args) {
-        System.getProperties().put("http.proxyHost", "localhost");
-        System.getProperties().put("http.proxyPort", "7890");
-
         PictureViewer.main(args);
     }
 }
